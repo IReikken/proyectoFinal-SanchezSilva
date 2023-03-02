b68f02929993237189efe4ed681893cb29de9ec7
Rodrigo Sanchez <rodrigo_im849@hotmail.com>
2023-03-01 23:52:37 -0600

    4to Commit proyecto final

--- script/fetch.js
+++ script/fetch.js
@@ -1,6 +1,8 @@
 // Cargar items de tienda desde archivo JSON
+data = []
 fetch('/data/stock.json')
     .then((resp)  => resp.json())
     .then(data => {
         mostrarItems(data)
+        items = data;
     })
